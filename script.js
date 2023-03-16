bac7549666694ef483d4be3d938ea226ff131945
Curtis <excentriceevee@hotmail.com>
2023-03-16 18:42:46 -0400

    Add stylesheet

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-div = document.querySelector(".container");
-div.textContent = "Hello World";
+div = document.querySelector('.container');
+
+// 16x16 is the etch-a-sketch grid size
+for(let i = 0; i < (16*16); i++) {
+    const box = document.createElement('div');
+    box.classList.toggle('box');
+    box.textContent = `${i}`;
+    //box.setAttribute('style', 'border: 1px solid black;');
+    div.appendChild(box);
+}
