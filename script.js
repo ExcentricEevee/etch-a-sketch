cd3e6268d8c69ebf6e63604178180f9ee8b7cb51
Curtis <excentriceevee@hotmail.com>
2023-03-20 19:58:04 -0400

    Have .box divs fill in on hover
    All style choices for the .box divs are appended in the script file,
    save for the .etched class which is in style.css. Boxes have gray borders
    for now in order to visualize the grid layout and further testing.

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ div = document.querySelector('.container');
 // 16x16 is the etch-a-sketch grid size
 for(let i = 0; i < (16*16); i++) {
     const box = document.createElement('div');
+
     box.classList.toggle('box');
-    box.textContent = `${i}`;
-    //box.setAttribute('style', 'border: 1px solid black;');
+    box.setAttribute('style', 'border: 1px solid gray; height: 10px; width: 10px;');
+
+    box.addEventListener('mouseover', () => {
+        box.classList.add('etched');
+    });
+
     div.appendChild(box);
 }
