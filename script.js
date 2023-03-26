127fc784c7acb4b9cae2b64d5876762664933f69
Curtis <excentriceevee@hotmail.com>
2023-03-25 22:23:33 -0400

    Add functionality that lets user pick grid size
    A button (New Grid) can be pressed by the user and will prompt a number.
    This number is used as the new grid dimensions, ex: user inputs 20 and
    the grid size will be 20x20.
    
    Also separating logic into functions; the initial grid simply
    calls on the makeGrid instead of having the for loop exposed raw, and
    the button clears the div children. Now it only needs to match the CSS.

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-div = document.querySelector('.container');
+div = document.querySelector('.grid-container');
+btn = document.querySelector('button');
 
-// 16x16 is the etch-a-sketch grid size
-for(let i = 0; i < (16*16); i++) {
-    const box = document.createElement('div');
+makeGrid(16);
 
-    box.classList.toggle('box');
-    box.setAttribute('style', 'border: 1px solid gray; height: 10px; width: 10px;');
+btn.addEventListener('click', () => {
+    let gridSize = Number(prompt('What do you want the grid size to be?'));
+    clearGrid();
+    makeGrid(gridSize);
+});
 
-    box.addEventListener('mouseover', () => {
-        box.classList.add('etched');
-    });
+function makeGrid(size) {
+    for (let i = 0; i < (size**2); i++) {
+        const box = document.createElement('div');
 
-    div.appendChild(box);
+        box.classList.toggle('box');
+        box.setAttribute('style', 'border: 1px solid gray; height: 10px; width: 10px;');
+
+        box.addEventListener('mouseover', () => {
+            box.classList.add('etched');
+        });
+
+        div.appendChild(box);
+    }
+}
+
+function clearGrid() {
+    while (div.firstChild) {
+        div.removeChild(div.firstChild);
+    }
 }
