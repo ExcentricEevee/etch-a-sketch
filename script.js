8a3e3884f4d4e541db56f933bba8463d5c5b910a
Curtis <excentriceevee@hotmail.com>
2023-03-29 23:41:24 -0400

    Change grid sizing to be static
    Previously the grid's boundaries would change based on grid size.
    However, this was due to my lack of understanding the Odin Project
    assignment. The grid has been changed to reflect this: regardless of the
    grid's column and row sizes, the total dimensions of the grid should
    stay the same.
    
    As a result, I have arbitrarily set the grid's height and width to 720px
    and thus the child divs within should change size to fit this container.
    For example, a 16x16 grid will have larger boxes while a 100x100 grid
    should have tiny boxes.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-div = document.querySelector('.grid-container');
+grid = document.querySelector('.grid-container');
 btn = document.querySelector('button');
 
+/* default grid size on load */
 makeGrid(16);
 
 btn.addEventListener('click', () => {
@@ -13,22 +14,19 @@ function makeGrid(size) {
     for (let i = 0; i < (size**2); i++) {
         const box = document.createElement('div');
 
-        box.classList.toggle('box');
-        box.setAttribute('style', 'border: 1px solid gray; height: 10px; width: 10px;');
-
         box.addEventListener('mouseover', () => {
             box.classList.add('etched');
         });
 
-        div.appendChild(box);
+        grid.appendChild(box);
     }
     
     /* .grid-container has its own styling in style.css, but we need the variable to make the size work */
-    div.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`);
+    grid.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`);
 }
 
 function clearGrid() {
-    while (div.firstChild) {
-        div.removeChild(div.firstChild);
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
     }
 }
