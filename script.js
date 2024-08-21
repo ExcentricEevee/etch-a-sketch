2cb039e3658e6f7828df35b78089b7edc72d7786
Curtis <45854361+ExcentricEevee@users.noreply.github.com>
2024-08-20 23:56:58 -0400

    Create 16x16 amount of square divs

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('hello world!');
+const container = document.querySelector('.container');
+const GRID_SIZE = 16 * 16;
+for(let i = 0; i < GRID_SIZE; i++) {
+    const square = document.createElement('div');
+    square.setAttribute('class', 'square');
+    container.appendChild(square);
+}
