35df7213a13ca9ec701680aaf30dc5d9ec9466de
Curtis <45854361+ExcentricEevee@users.noreply.github.com>
2024-08-21 01:48:34 -0400

    Organize divs into an actual 16 x 16 grid
    Rather than hardcoding dimensions, the container's height and width are
    programmatically determined based on the size of the children divs. This
    way it is possible to change the children div's values in CSS and not
    break the alignment of the grid. This will also be useful later when we
    add a feature to let the user pick their own grid size.

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const container = document.querySelector('.container');
-const GRID_SIZE = 16 * 16;
+const LENGTH = 16;
+const GRID_SIZE = LENGTH * LENGTH;
+let squareSize;
 for(let i = 0; i < GRID_SIZE; i++) {
     const square = document.createElement('div');
     square.setAttribute('class', 'square');
     container.appendChild(square);
+    // Need this to calculate container height/width later
+    squareSize = square.offsetWidth;
 }
+
+// This allows the .square divs to change size while maintaining the
+// square shape of the container, thanks to flex-wrap
+const containerSize = squareSize * LENGTH;
+container.setAttribute('style', `width: ${containerSize}px;
+    height: ${containerSize}px;`);
