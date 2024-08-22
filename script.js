5f4c1bdd82a1f62e41e0932ddeed5b15ddb66bc9
Curtis <45854361+ExcentricEevee@users.noreply.github.com>
2024-08-22 01:09:52 -0400

    Create function to clear grid
    The previous logic was delegated into its own function, createGrid, to
    prepare for the next feature of being able to make your own custom grid
    size. A clearGrid() function was implemented here in this commit to make
    sure whatever is already there doesn't overlap with the new grid.

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
-const container = document.querySelector('.container');
-const LENGTH = 16;
-const CONTAINER_SIZE = container.offsetWidth;
-const SQUARE_SIZE = CONTAINER_SIZE / LENGTH;
-
-for(let rows = 0; rows < LENGTH; rows++) {
-    const row = document.createElement('div');
-    row.setAttribute('class', 'row');
-    for(let squares = 0; squares < LENGTH; squares++) {
-        const square = document.createElement('div');
-        square.setAttribute('class', 'square');
-        square.setAttribute('style', `width: ${SQUARE_SIZE}px; height: ${SQUARE_SIZE}px;`);
-        square.addEventListener('mouseover', () => {
-            square.setAttribute('class', 'hovered');
-        });
-        row.appendChild(square);
+function createGrid(size) {
+    const LENGTH = size;
+    const container = document.querySelector('.container');
+    const CONTAINER_SIZE = container.offsetWidth;
+    const SQUARE_SIZE = CONTAINER_SIZE / LENGTH;
+    
+    for(let rows = 0; rows < LENGTH; rows++) {
+        const row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        for(let squares = 0; squares < LENGTH; squares++) {
+            const square = document.createElement('div');
+            square.setAttribute('class', 'square');
+            square.setAttribute('style', `width: ${SQUARE_SIZE}px; height: ${SQUARE_SIZE}px;`);
+            square.addEventListener('mouseover', () => {
+                square.setAttribute('class', 'hovered');
+            });
+            row.appendChild(square);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
+
+function clearGrid() {
+    const container = document.querySelector('.container');
+    while(container.lastElementChild) {
+        container.removeChild(container.lastElementChild);
+    }
+}
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+    //const size = prompt('What size grid? (Max 100)', '16');
+    clearGrid();
+});
+
+createGrid(16);
